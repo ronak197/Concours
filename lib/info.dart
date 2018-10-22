7900f732e338c945f5693cef7592422a61e550fd
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2018-10-23 02:49:28 +0530

    Added User info.dart file

---
+++ lib/info.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'firestore_config.dart';
+
+class InfoPage extends StatefulWidget {
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+
+  FirestoreConfig firestoreConfig;
+
+  @override
+  initState(){
+    super.initState();
+    firestoreConfig = new FirestoreConfig();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Information Page")
+    );
+  }
+}
